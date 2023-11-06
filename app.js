4900d934c1c1272aa71f380143308beea53af084
revati-kulkarni <88486875+revati-kulkarni@users.noreply.github.com>
2023-11-06 18:55:16 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Book = require("./models/book.model");
+const app = express();
+app.use(bodyParser.json());
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb+srv://revatikulkarni01:<root>@cluster0.2qtfn1l.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+// Get all books
+app.get("/getallbooks", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get a specific book by ID
+app.get("/getbookbyid/id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Add a new book
+app.post("/addnewbook", async (req, res) => {
+  const newBook = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    summary: req.body.summary,
+  });
+
+  try {
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Update a book by ID
+app.put("/updatebookbyid/id", async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete a book by ID
+app.delete("/deletbookbyid/:id", async (req, res) => {
+  try {
+    const deletedBook = await Book.findByIdAndDelete(req.params.id);
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// ...
